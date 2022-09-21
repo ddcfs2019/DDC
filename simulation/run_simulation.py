39d39be925137663eb222a340d2ab03d26e6dfc8
ddcfs2019 <58946583+ddcfs2019@users.noreply.github.com>
2022-09-21 17:39:56 -0400

    Update run_simulation.py

--- simulation/run_simulation.py
+++ simulation/run_simulation.py
@@ -3,14 +3,14 @@ import os,sys
 import numpy as np
 
 '''
-# generate 10 datasets
+# generate datasets
 os.system('python -W ignore discretize_generate_Y.py 100')
-for i in range(10):
+for i in range(100):
 	fname = 'data_S_100_5500_fdr/simulated_data_100_5500_'+str(i)+'.txt'
 	os.system('python -W ignore discretize_generate_simulated_data.py 100 5500 > ' + fname)
 
 os.system('python -W ignore discretize_generate_Y.py 1000')
-for i in range(10):
+for i in range(50):
 	fname = 'data_S_1000_170000_fdr/simulated_data_1000_170000_'+str(i)+'.txt'
 	os.system('python -W ignore discretize_generate_simulated_data.py 1000 170000 > ' + fname)
 
